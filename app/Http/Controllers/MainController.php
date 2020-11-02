eb94a52a8bc73272ec28de459486a67bd773b3d5
Serg Sivok <sivok26serg@amail.com>
2020-11-02 20:29:28 +0200

    third commit

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,26 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index() {
-        return view('index');
+        $products = Product::get();
+        return view('index', compact('products'));
     }
 
     public function categories() {
-        return view('categories');
+        $categories = Category::get();
+        return view('categories', compact('categories'));
     }
 
-    public function category($category) {
-
+    public function category($code) {
+        $category = Category::where('code', $code)->first();
         return view('category', compact('category'));
     }
 
-    public function product($product = null) {
+    public function product($category , $product = null) {
 
         return view('product', ['product' => $product]);
+    }
+
+    public function basket() {
+        return view('basket');
+    }
+
+    public function basketPlace() {
+        return view('order');
     }
 }
---
+++ resources/views/card.blade.php
@@ -0,0 +1,15 @@
+<div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+        <img src="https://i.allo.ua/media/catalog/product/cache/1/image/468x468/602f0fa2c1f0d1ba5e241f914e856ff9/i/p/iphone_11_b_2_2.jpg" alt="iPhone X 64GB">
+        <div class="caption">
+            <h3>{{ $product->name }}</h3>
+            <p>{{ $product->price }} грн.</p>
+            <p>
+                <a href="{{ route('basket') }}" class="btn btn-primary"
+                   role="button">В корзину</a>
+                <a href="{{ route('product', [$product->category->code, $product->code]) }}" class="btn btn-default"
+                   role="button">Подробнее</a>
+            </p>
+        </div>
+    </div>
+</div>
